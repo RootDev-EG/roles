57bca9e4500fb030315a9f14aa22f39d628e3beb
Mahmoud Alwakeel <m.wakeel@root-dev.com>
2017-12-12 14:05:01 +0200

    Fixing issue on laravel 5.5

--- src/Bican/Roles/Traits/HasRoleAndPermission.php
+++ src/Bican/Roles/Traits/HasRoleAndPermission.php
@@ -100,7 +100,7 @@ trait HasRoleAndPermission
      */
     public function hasRole($role)
     {
-        return $this->getRoles()->contains(function ($key, $value) use ($role) {
+        return $this->getRoles()->contains(function ($value) use ($role) {
             return $role == $value->id || Str::is($role, $value->slug);
         });
     }
